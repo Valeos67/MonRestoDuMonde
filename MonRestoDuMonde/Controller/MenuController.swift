a3c7688b4b7b88eadb6820f5592e3628204ae702
Valentin SCHERER <valentinscherer@MBP-de-Valentin.fritz.box>
2018-08-25 19:22:33 +0200

    Ajout des Controllers princpaux

---
+++ MonRestoDuMonde/Controller/MenuController.swift
@@ -0,0 +1,20 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Valentin SCHERER on 25/08/2018.
+//  Copyright © 2018 Valentin SCHERER. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+
+}
