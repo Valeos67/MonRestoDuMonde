767166727ab6b64f2746a0bebffe812c5a1d00f8
Valentin SCHERER <valentinscherer@MBP-de-Valentin.fritz.box>
2018-08-25 20:21:08 +0200

    Ajout Collection view

---
+++ MonRestoDuMonde/View/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by Valentin SCHERER on 25/08/2018.
+//  Copyright © 2018 Valentin SCHERER. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
