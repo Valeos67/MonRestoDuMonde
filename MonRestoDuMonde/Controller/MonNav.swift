660823b449e1b1a313604c011d582320fce02e0b
Valentin SCHERER <valentinscherer@MBP-de-Valentin.fritz.box>
2018-08-25 19:41:24 +0200

    Mon Application Controller

---
+++ MonRestoDuMonde/Controller/MonNav.swift
@@ -0,0 +1,22 @@
+//
+//  MonNav.swift
+//  MonRestoDuMonde
+//
+//  Created by Valentin SCHERER on 25/08/2018.
+//  Copyright © 2018 Valentin SCHERER. All rights reserved.
+//
+
+import UIKit
+
+class MonNav: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBar.barTintColor = NOIR
+        navigationBar.tintColor = GRIS_TRES_CLAIR
+        navigationBar.titleTextAttributes = [.font: FONT_DE_BASE as Any, .foregroundColor: GRIS_TRES_CLAIR]
+    }
+
+
+
+}
