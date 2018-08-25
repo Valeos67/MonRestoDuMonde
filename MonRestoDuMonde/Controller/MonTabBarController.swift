38abcfe784cf0ac7ac717f66b5c00c5a3e659e0a
Valentin SCHERER <valentinscherer@MBP-de-Valentin.fritz.box>
2018-08-25 19:32:34 +0200

    Tabbar custom

---
+++ MonRestoDuMonde/Controller/MonTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  MonTabBarController.swift
+//  MonRestoDuMonde
+//
+//  Created by Valentin SCHERER on 25/08/2018.
+//  Copyright © 2018 Valentin SCHERER. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+        
+        
+
+    }
+
+
+}
---
+++ MonRestoDuMonde/Utilitaires/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MonRestoDuMonde
+//
+//  Created by Valentin SCHERER on 25/08/2018.
+//  Copyright © 2018 Valentin SCHERER. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
